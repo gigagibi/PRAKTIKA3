9cc71e3e27aae6255992dd1cef0ca11eaebb2558
gigagibi <70891118+gigagibi@users.noreply.github.com>
2020-09-14 13:40:35 +0300

    Update Human.java

--- praktika3 zadanie2/src/com/company/Human.java
+++ praktika3 zadanie2/src/com/company/Human.java
@@ -1,18 +1,28 @@
 package com.company;
 
 public class Human {
-    private Hand hand = new Hand();
+    private Hand hand1 = new Hand();
+    private Hand hand2 = new Hand();
     private Head head = new Head();
-    private Leg leg = new Leg();
+    private Leg leg1 = new Leg();
+    private Leg leg2 = new Leg();
 
-    public Hand getHand() {
+    public Hand getHand1() {
         return hand;
     }
 
-    public void setHand(Hand hand) {
+    public Hand getHand2() {
+        return hand;
+    }
+    
+    public void setHand1(Hand hand) {
         this.hand = hand;
     }
 
+    public void setHand2(Hand hand) {
+        this.hand = hand;
+    }
+    
     public Head getHead() {
         return head;
     }
@@ -25,10 +35,24 @@ public class Human {
         return leg;
     }
 
-    public void setLeg(Leg leg) {
+    
+    public Leg getLeg1() {
+        return leg;
+    }
+    
+    
+    public Leg getLeg2() {
+        return leg;
+    }
+    
+    public void setLeg1(Leg leg) {
         this.leg = leg;
     }
 
+    public void setLeg2(Leg leg) {
+        this.leg = leg;
+    }
+    
     private int tall;
 
     public int getTall() {
